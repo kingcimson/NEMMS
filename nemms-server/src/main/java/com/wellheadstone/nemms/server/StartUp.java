fd8a1d5bb516df99045181aa145ca069c683a157
TomDeng <14068728@qq.com>
2015-06-28 17:57:07 +0800

    update

--- nemms-server/src/main/java/com/wellheadstone/nemms/server/StartUp.java
+++ nemms-server/src/main/java/com/wellheadstone/nemms/server/StartUp.java
@@ -2,7 +2,7 @@ package com.wellheadstone.nemms.server;
 
 public class StartUp {
 	public static void main(String[] args) {
-		int port = 8080;
+		int port = 9000;
 
 		if (args.length > 0) {
 			try {
