637cc3c90bfa5bea48722a3681cb808f9fa1113f
TomDeng <14068728@qq.com>
2015-07-03 19:41:22 +0800

    update

--- nemms-server/src/main/java/com/wellheadstone/nemms/server/StartUp.java
+++ nemms-server/src/main/java/com/wellheadstone/nemms/server/StartUp.java
@@ -1,6 +1,18 @@
 package com.wellheadstone.nemms.server;
 
+import org.apache.log4j.PropertyConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+import com.wellheadstone.nemms.common.util.PropertiesUtils;
+import com.wellheadstone.nemms.data.util.SpringContextUtils;
+
 public class StartUp {
+	protected final static Logger logger = LoggerFactory.getLogger(StartUp.class);
+	protected final static SpringContextUtils springContextUtil = new SpringContextUtils();
+
 	public static void main(String[] args) {
 		int port = 9000;
 
@@ -13,9 +25,20 @@ public class StartUp {
 		}
 
 		try {
-			new WebSocketServer().run(port);
+			init();
+			// new WebSocketServer().run(port);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	private static void init() {
+		PropertyConfigurator.configure("target/conf/log4j.properties");
+
+		ApplicationContext appContext = new FileSystemXmlApplicationContext("target/conf/app-config.xml");
+		springContextUtil.setApplicationContext(appContext);
+
+		PropertiesUtils.configure("target/conf/resource.properties");
+		System.out.println(PropertiesUtils.getValue("nemms.server.websocket.port"));
 	}
 }
