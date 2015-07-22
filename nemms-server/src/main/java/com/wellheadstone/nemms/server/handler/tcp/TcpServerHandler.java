916e56d87c32b9d0b019132894268dc01a6e57e7
TomDeng <14068728@qq.com>
2015-07-22 19:20:20 +0800

    update

--- nemms-server/src/main/java/com/wellheadstone/nemms/server/handler/tcp/TcpServerHandler.java
+++ nemms-server/src/main/java/com/wellheadstone/nemms/server/handler/tcp/TcpServerHandler.java
@@ -4,22 +4,22 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.socket.SocketChannel;
 
-import java.net.InetAddress;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TcpServerHandler extends SimpleChannelInboundHandler<String> {
+	private final static Logger logger = LoggerFactory.getLogger(TcpServerHandler.class);
+
 	@Override
 	public void channelActive(ChannelHandlerContext ctx) throws Exception {
+		logger.info("设备 : " + ctx.channel().remoteAddress() + "连接通道已经激活");
 		TcpSocketChannelMap.add("192.168.10.67", (SocketChannel) ctx.channel());
-		System.out.println("RamoteAddress : " + ctx.channel().remoteAddress() + " active !");
-		ctx.writeAndFlush("Welcome to " + InetAddress.getLocalHost().getHostName() + " service!\n");
 		super.channelActive(ctx);
 	}
 
 	@Override
 	protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
-		// 收到消息直接打印输出
-		System.out.println(ctx.channel().remoteAddress() + " Say : " + msg);
-		// 返回客户端消息 - 我已经接收到了你的消息
+		logger.info("接收到设备: " + ctx.channel().remoteAddress() + "的数据：" + msg);
 		ctx.writeAndFlush("Received your message !\n");
 	}
 }
