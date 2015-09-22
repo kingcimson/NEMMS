1aa90b88894960930a61273612eadbd79735e463
智恒 <tomdeng.dzw@alibaba-inc.com>
2015-09-22 23:12:11 +0800

    重构读取配置项的代码

--- nemms-server/src/main/java/com/wellheadstone/nemms/server/MySocketIOServer.java
+++ nemms-server/src/main/java/com/wellheadstone/nemms/server/MySocketIOServer.java
@@ -7,7 +7,6 @@ import org.slf4j.LoggerFactory;
 
 import com.corundumstudio.socketio.Configuration;
 import com.corundumstudio.socketio.SocketIOServer;
-import com.wellheadstone.nemms.common.util.PropertiesUtils;
 import com.wellheadstone.nemms.server.handler.socketio.EventName;
 import com.wellheadstone.nemms.server.handler.socketio.ListenerFactory;
 import com.wellheadstone.nemms.server.message.SocketIOMessage;
@@ -18,8 +17,8 @@ public class MySocketIOServer implements IServer {
 	@Override
 	public void start() {
 		Configuration config = new Configuration();
-		config.setHostname(this.getIPAddress());
-		config.setPort(this.getPort());
+		config.setHostname(Config.getServerIP());
+		config.setPort(Config.getSocketIOPort());
 
 		final SocketIOServer server = new SocketIOServer(config);
 		server.addEventListener(EventName.GetParamList, SocketIOMessage.class,
@@ -39,25 +38,5 @@ public class MySocketIOServer implements IServer {
 		} finally {
 			server.stop();
 		}
-	}
-
-	private String getIPAddress() {
-		String ipAddr = "localhost";
-		try {
-			ipAddr = PropertiesUtils.getValue("nemms.server.ip").trim();
-		} catch (Exception e) {
-			logger.error("SocketIO Server IP Parse Error,Set the default IP:" + ipAddr, e);
-		}
-		return ipAddr;
-	}
-
-	private int getPort() {
-		int port = 9100;
-		try {
-			port = Integer.parseInt(PropertiesUtils.getValue("nemms.server.socketio.port").trim());
-		} catch (Exception e) {
-			logger.error("SocketIO Server Port Parse Error,Set the default port:" + port, e);
-		}
-		return port;
 	}
 }
